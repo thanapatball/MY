eb1d840b5d86249f1ddffa0a42e3350c41c3c57c
thanapatball <59791443+thanapatball@users.noreply.github.com>
2023-07-10 10:39:29 +0700

    Add files via upload

---
+++ NEW1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{   cout << "Student code : 66-060216-2120-8"<<endl;
+	cout << "Student Name : Thanapat  Ousungnoen"<<endl;
+	cout << "Division     : Information Technology"<<endl;
+	cout <<	"Year         : 1"<<endl;
+	cout << "Room	     : RC"<<endl;
+	cout << "sex	     : Male"<<endl;
+    cout << "Age	     : 18"<<endl;
+	system("pause");
+	return 0;
+}
