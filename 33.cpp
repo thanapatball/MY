b6d51c7023166491f93a54952882e07151231c61
thanapatball <59791443+thanapatball@users.noreply.github.com>
2023-07-17 10:04:26 +0700

    Add files via upload

---
+++ 33.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{	string Name;
+	int number1,number2;
+	cout<< "Enter name  : ";
+	cin >>Name;
+	cout<<"Enter Year of birth:";
+	cin >>number1;
+	cout<<"Enter second number";
+	cin >>number2;
+	cout<<endl;
+	cout<<"sum"<<number1<<"-"<<number2;
+	cout<<"="<<number1-number2<<"year old."<<endl;
+	return(0);
+
+	
+}
